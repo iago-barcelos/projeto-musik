99c673ef6b2246b8deef662b90bf30320f96c84b
Iago Barcelos <iago.cmpb@gmail.com>
2023-08-10 15:54:53 -0300

    requisito 2

--- src/pages/album/album.tsx
+++ src/pages/album/album.tsx
@@ -1,6 +1,31 @@
-function Album() {
+import { Link } from 'react-router-dom';
+import { AlbumType } from '../../types';
+
+type AlbumTypeProps = {
+  album: AlbumType,
+};
+
+function Album({ album }: AlbumTypeProps) {
+  const {
+    artistName,
+    artworkUrl100,
+    collectionId,
+    collectionName,
+  } = album;
+
   return (
-    <h1>Album</h1>
+    <div>
+      <img src={ artworkUrl100 } alt={ collectionName } />
+      <p>{collectionName}</p>
+      <p>{artistName}</p>
+      <Link
+        to={ `/album/${collectionId}` }
+        data-testid={ `link-to-album-${collectionId}` }
+      >
+        Ver Álbum
+
+      </Link>
+    </div>
   );
 }
 
--- src/pages/search/search.tsx
+++ src/pages/search/search.tsx
@@ -1,6 +1,94 @@
+import { useState } from 'react';
+import searchAlbumsAPI from '../../services/searchAlbumsAPI';
+import { AlbumType } from '../../types';
+import Loading from '../../components/loading/loading';
+import Album from '../album/album';
+
+type FormValuesTypes = {
+  term: string,
+};
+
+const initialFormValues = {
+  term: '',
+};
+
 function Search() {
+  const [formValues, setFormValues] = useState<FormValuesTypes>(initialFormValues);
+  const [albums, setAlbums] = useState<AlbumType[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [searchedArtist, setSearchedArtist] = useState<string>('');
+
+  function handleChange(
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>,
+  ) {
+    const { name, type } = event.target;
+    const value = type === 'checkbox'
+      ? (event.target as HTMLInputElement).checked
+      : event.target.value;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  }
+
+  const handleSubmit = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault();
+    setLoading(true);
+    const data = await searchAlbumsAPI(formValues.term);
+    console.log(data);
+    setAlbums(data);
+    setSearchedArtist(formValues.term);
+    setFormValues(initialFormValues);
+    setLoading(false);
+  };
+
+  const validateForm = () => formValues.term.length >= 2;
+
+  if (loading) {
+    return (
+      <Loading />
+    );
+  }
   return (
-    <h1>Search</h1>
+    <>
+      <div>Search</div>
+      <form>
+        <input
+          data-testid="search-artist-input"
+          id="term"
+          name="term"
+          onChange={ handleChange }
+          type="text"
+        />
+        <button
+          data-testid="search-artist-button"
+          disabled={ !validateForm() }
+          onClick={ handleSubmit }
+          type="submit"
+        >
+          Pesquisar
+        </button>
+      </form>
+      {albums.length > 0 && (
+        <div>
+          <p>
+            Resultado de álbuns de:
+            {' '}
+            {searchedArtist}
+          </p>
+          <div>
+            {albums.map((album) => (
+              <Album album={ album } key={ album.collectionId } />
+            ))}
+          </div>
+        </div>
+      )}
+      {albums.length === 0 && searchedArtist.length > 1 && (
+        <div>
+          <p>Nenhum álbum foi encontrado</p>
+        </div>
+      )}
+    </>
   );
 }
 
